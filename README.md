12c50c95986984441f72bbd2b88514f55b388398
edenhliu <edenhliu@tencent.com>
2018-10-30 19:33:30 +0800

    refactor: refactor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # eslint-config-zman
+
+基于 `eslint-config-airbnb` 的自定义 eslint 配置
--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,14 +1,8 @@
-const pkgs = [
-  'babel-eslint',
-  'eslint eslint-config-airbnb',
-  'eslint-plugin-babel',
-  'eslint-plugin-import',
-  'eslint-plugin-jsx-a11y',
-  'eslint-plugin-react',
-];
+const pkg = require('../package.json');
+
 console.log(`
 if you install into global, you need exc this command
 
-  npm i -g ${pkgs.join(' ')}
+  npm i -g ${Object.keys(pkg.dependencies).join(' ')}
 
 `);
