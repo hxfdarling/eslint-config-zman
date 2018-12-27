688e1c1451135d99ce3da26a40f7ae8b187bd164
edenhliu <edenhliu@tencent.com>
2018-12-27 13:35:40 +0800

    fix: 修复eslint和prettier冲突问题

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = {
     },
     ecmaVersion: 6,
   },
-  plugins: ['react', 'babel'],
+  plugins: ['react', 'babel', 'prettier'],
   rules: {
     'array-callback-return': 0,
     'arrow-body-style': 0,
